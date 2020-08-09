501e1adb1540e6cfa1cbec008a827536f3dfaf5e
andrey067 <audrey.ernesto.lima@gmail.com>
2020-08-09 10:49:35 -0400

    novos exercicios

---
+++ Exemplo_Heranca/Exemplo_Heranca/Entities/SavingsAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exemplo_Heranca.Entities
+{
+    class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount() { }
+
+        public SavingsAccount(int number, string holder, double balance,double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+
+        }
+        public void updateBalance() {
+            Balance += Balance * InterestRate;
+        }
+
+        public override void Withdraw(double amount)
+        {
+            base.Withdraw(amount);
+            Balance -= 2.0;
+        }
+    }
+}
